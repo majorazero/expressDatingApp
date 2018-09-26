ab3e4be8e3b1c43ecfd4295f9615fa4464b751cb
majorazero <daniel.cw.hsu@gmail.com>
2018-09-26 10:09:09 -0700

    reorganized javascript file

---
+++ app/javascript/survey.js
@@ -0,0 +1,28 @@
+$("#questionaire").on("submit",function(event){
+  event.preventDefault();
+  let answersArray = [];
+  for(let i = 0; i < 10; i++){
+    if($("#q"+(i+1)).val() === ""){
+      answersArray.push(0);
+    }
+    else {
+      answersArray.push(parseInt($("#q"+(i+1)).val()));
+    }
+  }
+  let photoUrl = $("#photoInp").val();
+  if(photoUrl === ""){
+    photoUrl = "https://picsum.photos/200/300?image=55";
+  }
+  let obj = {
+    name: $("#nameInp").val() ,
+    score: answersArray,
+    photo: photoUrl
+  }
+  $.post("/api/friends",obj).then(function(data){
+    $("#soulBod").empty();
+    $("#soulBod").append("<h1>Your soulmate is..."+data[0].name+"!</h1>");
+    $("#soulBod").append("<img src='"+data[0].picture+"' alt='' />");
+    $("#soulBod").append("<h2>Your have "+((40-data[1])/40)*100+"% match!</h2>");
+    $("#matchModal").modal('show');
+  });
+})
